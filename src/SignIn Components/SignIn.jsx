559ca146d16cdb7d187f45035e5c284b7dc5803e
ChoforJr <choforforsakang@gmail.com>
2025-12-06 23:33:29 +0100

    configured the signIn page functionality
    created and connected the login state to the login values on the
    page, created the submit button that has the onClick function of
    handleSubmit that will send the values to the backend using the
    fetch api and then storing the user token in localStorage, also
    I made some sylying changes in on the homepage and the signIn page

--- src/SignIn Components/SignIn.jsx
+++ src/SignIn Components/SignIn.jsx
@@ -1,16 +1,73 @@
-import { Link } from "react-router-dom";
 import styles from "./signIn.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const apiUrl = import.meta.env.VITE_BLOG_API_URL;
 
 const SignIn = () => {
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  });
+
+  function onChangeHandler(event) {
+    const { name, value } = event.target;
+    setLogin((prevLogin) => ({
+      ...prevLogin,
+      [name]: value,
+    }));
+  }
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (login.username == "" || login.password == "") {
+      return alert("You need to fill in all the field");
+    }
+
+    try {
+      const response = await fetch(`${apiUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(login),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("authorization", data.token);
+        navigate("/account", { replace: true });
+      } else {
+        alert("Login failed. Please check your credentials.");
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+    }
+  };
   return (
     <div className={styles.signIn}>
-      <Link to="/">
-        <h1>
-          WELCOME TO
-          <br />
-          SignIn
-        </h1>
-      </Link>
+      <label htmlFor="username">
+        Username:{" "}
+        <input
+          type="email"
+          name="username"
+          id="username"
+          value={login.username}
+          onChange={onChangeHandler}
+        />
+      </label>
+      <label htmlFor="password">
+        Password:{" "}
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={login.password}
+          onChange={onChangeHandler}
+        />
+      </label>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
